b67470115099707d83e59a571e2cf0bbd542478a
Ayan Das <ayandas9531@gmail.com>
2022-06-20 04:08:21 +0530

    corrected some.java

--- some.java
+++ some.java
@@ -1,16 +1,16 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class some {
-     public static int romanToInt(String s) {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("I", 1);
-        map.put("V",5);
-        map.put("X",10);
-        map.put("L",50);
-        map.put("C",100);
-        map.put("D",500);
-        map.put("M",1000);
+class some {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
         
         int result=map.get(s.charAt(0));
         for(int i=1;i<s.length();i++){
@@ -22,22 +22,5 @@ public class some {
             }
         }
         return result;
-    }
-    public static void main(String[] args) {
-        // Map<String, Integer> map = new HashMap<>();
-        // map.put("I", 1);
-        // map.put("V",5);
-        // map.put("X",10);
-        // map.put("L",50);
-        // map.put("C",100);
-        // map.put("D",500);
-        // map.put("M",1000);
-        
-        // System.out.println(map.get("I"));
-        // String s = "XVII";
-        // System.out.println(s.length());
-        String a = "III";
-        int n = romanToInt(a);
-        System.out.println(n);
     }
 }
